07a85bd626807c19d4d0e19799a4f2cf7f2060ff
Arjun Narayan <arjunn11@gmail.com>
2016-12-02 18:11:53 -0500

    finished basic analysis, output results to file

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@ import csv
 import pandas as pd
 import numpy as np
 import math
+import sys
+
 def text_to_csv():
     with open('syntheticCDR.txt', 'r') as in_file:
         #remove whitespace
@@ -16,44 +18,66 @@ def process():
     df = pd.read_csv('syntheticCDR.csv')
     #Strip whitespace from DataFrame headers (column titles).
     df.rename(columns=lambda x: x.strip(), inplace=True)
+    df['DateTime'] = pd.to_datetime(df['DateTime'])
+    #df['LineID.From'] = pd.to_
     #print(df.head())
     #print(df.dtypes)
 
+    #Set output to file.
+    sys.stdout = open('analysis.txt', 'w')
+
     #Calculate total charged over this period of time.
     #.loc() handles indexing
-    call_charge = df.loc[df['Transaction'] == 'call', 'Charge'].sum()
-    sms_charge = df.loc[df['Transaction'] == 'sms', 'Charge'].sum()
-    total_charge = call_charge + sms_charge
-    print('Total Charge: ', total_charge, '\n')
+    total_charge = df.loc[(df.Transaction == 'call') | (df.Transaction == 'sms'), 'Charge'].sum()
+    print('Questions:\n\n1. Total Charge: ', total_charge)
 
     #Find most popular handset model.
     #Total handsets
     models_df = df[['HandsetID.From', 'HandsetModel.From']].drop_duplicates('HandsetID.From')
     most_popular = models_df['HandsetModel.From'].mode()[0]
-    model_counts = models_df['HandsetModel.From'].value_counts()
+    max_count = models_df['HandsetModel.From'].value_counts().max()
     #Includes handsets which have models (ones which initiated transactions).
     total_handsets = models_df.count()[0]
-    print('Most popular handset model:', most_popular)
-    print("Count:", model_counts.max(), "| Total models:", total_handsets, '\n')
+    print('\n2. Most popular handset model:', most_popular)
+    print(" + Count:", max_count, "| Total handsets:", total_handsets)
 
-    #Find mean number of balance inquiries per person.
+    #Find mean number of balance inquiries per person:
     num_balance_inquiries = df.loc[df['Transaction'] == 'balance inquiry'].count()[0]
-    print("Mean number of balance inquiries per person: ", num_balance_inquiries, '\n')
+    print("\n3. Mean number of balance inquiries per person: ", num_balance_inquiries)
 
-    #Mean degree of contacts per person (incoming and outgong)
-    print('Assumption: Contacts is defined as calls and sms.')
-    calls_df = df[df['Transaction'] == 'call']
-    sms_df = df[df['Transaction'] == 'sms']
-    #Multiply each count by two to include both incoming and outgoing.
-    total_contacts = sms_df.count()[0]*2 + calls_df.count()[0]*2
+    #Create DataFrame of contacts (defined as sms or call transactions)
+    contacts_df = df[(df['Transaction'] == 'call') | (df['Transaction'] == 'sms')]
+
+    #Mean degree of contacts per person (incoming and outgong):
+    #Multiply count by two to include both incoming and outgoing.
+    total_contacts = contacts_df.count()[0]*2
     #Find number of unique people/handsets.
-    print('Note: 2 people dont make any transactions (only in LineId.To)')
-    unique_contacts = np.unique(df[['LineID.From', 'LineID.To']])
-    #remove NaN values
-    unique_contacts = unique_contacts[~np.isnan(unique_contacts)]
-    num_unique_contacts = len(unique_contacts)
-    mean_contacts = total_contacts / num_unique_contacts
-    print("Mean degree of contacts per person: ", mean_contacts)
+    unique_people = np.unique(df[['LineID.From', 'LineID.To']])
+    #remove NaN values (which come from 'LineID.To')
+    unique_people = unique_people[~np.isnan(unique_people)]
+    num_unique_people = len(unique_people)
+    mean_contacts = total_contacts / num_unique_people
+    print('\n4. Mean degree of contacts per person: ', mean_contacts)
+    print(' + Assumption: Contacts is defined as calls and sms.')
+    print(' + Note: 2 people dont make any transactions (only in LineId.To)')
+
+    #Asymmetry:
+    #Use contacts_df so topups and balance inquries are filtered out.
+    outgoing_frequency = contacts_df['LineID.From'].value_counts().sort_index()
+    #first remove NaN values from LineId.To
+    incoming_contacts = contacts_df['LineID.To'][~np.isnan(df['LineID.To'])]
+    incoming_frequency = incoming_contacts.astype(int).value_counts().sort_index()
+    person_asymmetry = outgoing_frequency - incoming_frequency
+    #print(person_asymmetry.isnull())
+    #Remove nulls from Series
+    person_asymmetry = person_asymmetry[~np.isnan(person_asymmetry)]
+    #Convert frequency values from float to integer
+    person_asymmetry = person_asymmetry.astype(int)
+    max_asymmetry = person_asymmetry.abs().idxmax()
+    print('\n5. Person with largest asymmetry: ', max_asymmetry)
+    print(' + Level of asymmetry: ', person_asymmetry[max_asymmetry], '(Outgoing - Incoming Contacts)')
+
+    print('\n--------\n\nAdditional Analysis')
 
 def main():
     process()
